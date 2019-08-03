c7e26460c10f447edc33baea216ede1822c8f35a
Ricardo Belo <iesp@ricardobelo.com.br>
2019-08-03 16:07:50 -0300

    Pacientes, Usuários, Funcionários(com verificação de e-mail)

--- src/main/java/br/com/consultemed/repository/repositories/FuncionarioRepository.java
+++ src/main/java/br/com/consultemed/repository/repositories/FuncionarioRepository.java
@@ -12,30 +12,31 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 
-import br.com.consultemed.models.Usuario;
+import br.com.consultemed.models.Funcionario;
+import br.com.consultemed.models.Paciente;
 import br.com.consultemed.utils.JPAUtils;
 
 /**
  * @author carlosbarbosagomesfilho
  *
  */
-public class UsuarioRepository {
+public class FuncionarioRepository {
 
 	EntityManagerFactory emf = JPAUtils.getEntityManagerFactory();
 	EntityManager factory = emf.createEntityManager();
-	
-	public List<Usuario> listaUsuarios() {
-		Query query = this.factory.createQuery("SELECT object(u) FROM Usuario as u");
+
+	public List<Funcionario> listaFuncionario() {
+		Query query = this.factory.createQuery("SELECT object(f) FROM Funcionario as f");
 		return query.getResultList();
 	}
 
-	public Collection<Usuario> listarUsuarios() throws Exception {
+	public Collection<Funcionario> listarFuncionarios() throws Exception {
 		this.factory = emf.createEntityManager();
-		List<Usuario> contatos = new ArrayList<Usuario>();
+		List<Funcionario> funcionarios = new ArrayList<Funcionario>();
 		try {
 			factory.getTransaction().begin();
-			TypedQuery<Usuario> query = factory.createNamedQuery("Usuario.findAll", Usuario.class);
-			contatos = query.getResultList();
+			TypedQuery<Funcionario> query = factory.createNamedQuery("Funcionario.findAll", Funcionario.class);
+			funcionarios = query.getResultList();
 			factory.getTransaction().commit();
 
 		} catch (Exception e) {
@@ -45,17 +46,17 @@ public class UsuarioRepository {
 			factory.close();
 		}
 
-		return contatos;
+		return funcionarios;
 	}
 
-	public void salvarUsuario(Usuario usuario) {
+	public void salvarFuncionario(Funcionario funcionario) {
 		this.factory = emf.createEntityManager();
 		try {
 			factory.getTransaction().begin();
-			if (usuario.getId() == null) {
-				factory.persist(usuario);
+			if (funcionario.getId() == null) {
+				factory.persist(funcionario);
 			} else {
-				factory.merge(usuario);
+				factory.merge(funcionario);
 			}
 			factory.getTransaction().commit();
 		} catch (Exception e) {
@@ -69,13 +70,13 @@ public class UsuarioRepository {
 
 	public void deleteById(Long id) throws Exception {
 		this.factory = emf.createEntityManager();
-		Usuario usuario = new Usuario();
+		Funcionario funcionario = new Funcionario();
 
 		try {
 
-			usuario = factory.find(Usuario.class, id);
+			funcionario = factory.find(Funcionario.class, id);
 			factory.getTransaction().begin();
-			factory.remove(usuario);
+			factory.remove(funcionario);
 			factory.getTransaction().commit();
 
 		} catch (Exception e) {
@@ -86,6 +87,20 @@ public class UsuarioRepository {
 		}
 
 	}
-
+	
+	public boolean getFuncionarioByEmail(String email) {
+		try {
+		
+			String jpql = "SELECT object(f) FROM Funcionario as f WHERE f.email = :emailFuncionario";
+			Query query = this.factory.createQuery(jpql); 
+			query.setParameter("emailFuncionario", email);
+			
+			if(query.getSingleResult() != null) {
+				return true;
+			}else {return false;}
+		}catch (Exception e) {
+			return false;
+		}
+	}
 
 }
--- src/main/java/br/com/consultemed/services/UsuarioService.java
+++ src/main/java/br/com/consultemed/services/UsuarioService.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import br.com.consultemed.models.Medico;
 import br.com.consultemed.models.Usuario;
 import br.com.consultemed.repository.repositories.UsuarioRepository;
 
 /**
  * @author carlosbarbosagomesfilho
+ * @co Ricardo Belo
  *
  */
 public class UsuarioService {
@@ -22,4 +24,15 @@ public class UsuarioService {
 	public List<Usuario> listaUsuarios(){
 		return this.dao.listaUsuarios();
 	}
+
+	public void salvarUsuario(Usuario usuario) {
+		this.dao.salvarUsuario(usuario);
+	}
+	
+	public void deletarUsuario(Long id) throws Exception {
+		this.dao.deleteById(id);
+	}	
+	
 }
+
+
